8afb58a3d90ae7d18f996a17447820d3acc1afe6
Seacity <>
2022-03-22 19:47:50 +0800

    轮播

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,33 +5,79 @@
         <div>首页</div>
       </template>
     </nav-bar>
+
+    <template>
+      <swiper class="swiper" 
+        :modules="modules"
+        :space-between="30"
+        :effect="'fade'"
+        :navigation="true">
+        <swiper-slide class="slide" v-for="(item,index) in banner" :key="index">
+          <!-- <img :src="url" alt=""> -->
+          <div>slide</div>
+        </swiper-slide>
+      </swiper>
+    </template>
+
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/common/navbar/Navbar.vue'
+import { Pagination, Navigation, EffectFade } from 'swiper'
+import { Swiper, swiperSlide ,useSwiper } from 'swiper/vue'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import 'swiper/css/effect-fade'
 import { getHomeData } from '@/network/home'
 export default {
   data(){
     return{
-      result:null
+       banner:[],
+       commends:[],
+       url:"https://www.baidu.com/img/baidu_jgylogo3.gif"
     }
   },
   created (){
+    this.initSwiper()
     getHomeData().then(res=>{
-      this.result = res
-      console.log(this.result);
+      this.banner = res.data.banner.list
     })
   },
   components:{
-    NavBar
+    NavBar,
+    Swiper,
+    swiperSlide
+  },
+  setup(){
+    return{
+      modules:[Pagination, Navigation, EffectFade]
+    }
+  },
+  methods:{
+    initSwiper(){
+      const swiper = useSwiper()
+      return { swiper }
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 .home-nav {
   background-color: var(--color-tint);
   color: white
+}
+
+.swiper {
+  /* flex: 1; */
+  height: 180px;
+  background-color: red;
+}
+
+.slide {
+  width: 300px;
+  height: 180px;
 }
 </style>
