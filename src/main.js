6945e3e569f689c216d2474195340b59c2ced79c
Seacity <>
2022-04-01 17:08:29 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router/index.js'
 import store from './store'
 import toast from '@/components/common/toast/index'
+import FastClick from 'fastclick'
+
+FastClick.attach(document.body)
 
 const app = createApp(App)
 app.use(router)
